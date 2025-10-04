c736ba349decaaf73eb2c3f257356064bc37b2c6
Balneo <fritz.forsgren@gmail.com>
2025-10-04 18:13:44 +0200

    add the ones i forget to add 2 commits ago

---
+++ i3blocks/.config/i3blocks/nbattery.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# ~/.config/i3blocks/nbattery
+# Nerd Font battery icons
+#
+
+#
+# Get the battery info
+#
+
+battery_info=$(acpi -b)
+status=$(echo "$battery_info" | awk -F ', ' '{print $1}')
+perc=$(echo "$battery_info" | awk -F ', ' '{print $2}' | tr -d '%')
+
+# Determine icon
+icon=""
+if [[ "$status" == *"Charging" ]]; then
+    icon="\uF0E7"   # ⚡ Charging
+elif  [ $perc -ge 80 ]; then
+    icon="\uF240"  # 
+elif [ $perc -ge 60 ]; then
+    icon="\uF241"  # 
+elif [ $perc -ge 40 ]; then
+    icon="\uF242"  # 
+elif [ $perc -ge 20 ]; then
+    icon="\uF243"  # 
+else
+    icon="\uF244"  # 
+fi
+
+#
+# Print output
+#
+echo -e "$icon $perc%  "
