bced5c047184754010afbac29f64c53212513d21
Balneo <fritz.forsgren@gmail.com>
2025-10-10 23:15:14 +0200

    and change to 20% as the first alert

--- scripts/.local/bin/battery_alert.sh
+++ scripts/.local/bin/battery_alert.sh
@@ -6,7 +6,7 @@ STATUS=$(cat $BAT/status)
 if [[ "$STATUS" == "Discharging" ]]; then
     if (( LEVEL <= 10 )); then
         notify-send -u critical "Battery critically low" "Level: ${LEVEL}%"
-    elif (( LEVEL <= 200 )); then
+    elif (( LEVEL <= 20 )); then
         notify-send -u normal "Battery low" "Level: ${LEVEL}%"
     fi
 fi
