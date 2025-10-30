fbd89e3804502468bf64c4cfe36f3f71444dbb43
Balneo <fritz.forsgren@gmail.com>
2025-10-30 22:12:29 +0100

    add full workspace support per monitor

---
+++ hypr/.config/hypr/scripts/switch_workspace.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+operation=$1
+workspace=$2
+
+monitor_id=$(hyprctl activeworkspace -j | jq -r ".monitorID")
+workspace_id=$(($monitor_id * 10 + $workspace))
+echo "Final Operation: $operation to $workspace_id"
+
+if [[ $operation == "switch" ]]; then
+    hyprctl dispatch moveworkspacetomonitor $workspace_id $monitor_id;
+    hyprctl dispatch workspace $workspace_id;
+    hyprctl dispatch renameworkspace "$workspace_id" "$workspace"
+fi
+
+if [[ $operation == "move" ]]; then
+    hyprctl dispatch moveworkspacetomonitor $workspace_id $monitor_id;
+    hyprctl dispatch movetoworkspace $workspace_id;
+fi
+
+
